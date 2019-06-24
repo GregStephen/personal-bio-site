5264861251672c2dfbc41f83dcd922f804b67b94
Greg Stephen <GregAStephen@gmail.com>
2019-06-24 18:10:26 -0500

    fixed navbar button issue

--- public/javascripts/components/Navbar/Navbar.js
+++ public/javascripts/components/Navbar/Navbar.js
@@ -4,7 +4,7 @@ const pages = $('.fullPage');
 
 const eventListeners = () => {
   $('#navbar').on('click', '.navLink', (e) => {
-    const target = $(e.target);
+    const target = $(e.currentTarget);
     const pageToLoad = (target.data('page'));
     pages.addClass('hide');
     $(`#${pageToLoad}`).removeClass('hide');
@@ -24,7 +24,7 @@ const buildNavbar = () => {
   domstring += '<hr>';
   domstring += '<div class="navigationList">';
   domstring += '<ul id="navLinks">';
-  domstring += '<li><a class="navLink" data-page="bioPage" id="navToBio"> <span class="fas fa-user"></span> Bio</a></li>';
+  domstring += '<li><a class="navLink" data-page="bioPage" id="navToBio"> <i class="fas fa-user"></i> Bio</a></li>';
   domstring += '<li><a class="navLink" data-page="technologiesPage" id="navToTechnologies"><span class="fas fa-file-code"></span> Technologies</a></li>';
   domstring += '<li><a class="navLink" data-page="projectsPage" id="navToProjects"><span class="fas fa-cogs"></span> Projects</a></li>';
   domstring += '</ul>';
