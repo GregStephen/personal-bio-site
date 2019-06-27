4521e740a0d6044f0a4fd77f16669190d8b12b07
Greg Stephen <GregAStephen@gmail.com>
2019-06-27 01:28:00 -0500

    changes color and adds textshadow to selected nav button

--- public/javascripts/components/Navbar/Navbar.js
+++ public/javascripts/components/Navbar/Navbar.js
@@ -1,16 +1,21 @@
 import util from "../../helpers/util.js";
 
 const pages = $('.fullPage');
+// const navLinks = $('.navLink');
 
 const eventListeners = () => {
   $('#navbar').on('click', '.navLink', (e) => {
     const target = $(e.currentTarget);
+    $('.navLink').removeClass('selected-nav');
+    target.addClass('selected-nav');
     const pageToLoad = (target.data('page'));
     pages.addClass('hide');
     $(`#${pageToLoad}`).removeClass('hide');
   });
   $('#bioPage').on('click', '.navLink', (e) => {
     const target = $(e.target);
+    $('.navLink').removeClass('selected-nav');
+    $('#navToProjects').addClass('selected-nav');
     const pageToLoad = (target.data('page'));
     pages.addClass('hide');
     $(`#${pageToLoad}`).removeClass('hide');
@@ -24,7 +29,7 @@ const buildNavbar = () => {
   domstring += '<hr>';
   domstring += '<div class="navigationList">';
   domstring += '<ul id="navLinks">';
-  domstring += '<li><a class="navLink" data-page="bioPage" id="navToBio"> <i class="fas fa-user"></i> Bio</a></li>';
+  domstring += '<li><a class="navLink selected-nav" data-page="bioPage" id="navToBio"> <i class="fas fa-user"></i> Bio</a></li>';
   domstring += '<li><a class="navLink" data-page="technologiesPage" id="navToTechnologies"><span class="fas fa-file-code"></span> Technologies</a></li>';
   domstring += '<li><a class="navLink" data-page="projectsPage" id="navToProjects"><span class="fas fa-cogs"></span> Projects</a></li>';
   domstring += '</ul>';
